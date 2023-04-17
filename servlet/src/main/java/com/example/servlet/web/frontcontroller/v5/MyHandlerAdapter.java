142a3a780199a724d246884d78b215019fd0bd32
Kwon yongho <b2b2004@nate.com>
2023-04-17 20:42:58 +0900

    4.MVC Framework v4,v5 add

---
+++ servlet/src/main/java/com/example/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package com.example.servlet.web.frontcontroller.v5;
+
+import com.example.servlet.web.frontcontroller.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    // 어댑터가 해당 컨트롤러를 처리할 수 있는지 판단하는 메서드다.
+    boolean supports(Object handler);
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
---
+++ servlet/src/main/java/com/example/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.servlet.web.frontcontroller.v5.adapter;
+
+import com.example.servlet.web.frontcontroller.ModelView;
+import com.example.servlet.web.frontcontroller.v3.ControllerV3;
+import com.example.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3); // V3의 인스턴스니? ok --> true
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        ControllerV3 controller = (ControllerV3) handler; // V3
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+        return mv;
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
